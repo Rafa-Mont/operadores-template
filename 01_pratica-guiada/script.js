a6721148199dd5b7e3901fc3984d606284a0f21b
Rafa-Mont <rafamont6891@gmail.com>
2023-02-07 21:40:53 -0300

    operadores-fixacao-guiada-resolvido

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,61 @@
 //CODIGO AQUI
+// primeira pratica guiada
+
+let a = 15
+let b = 22.40
+let c = 12.34
+
+console.log(a + b - c)
+
+let d = 25.6
+let e = 2
+
+console.log(d/e)
+
+let f = 12.8
+let g = 3.42
+
+console.log(f*g)
+
+let h = 43.776
+let i = 3
+console.log(h/i)
+
+let j = 0.592
+console.log(j*i)
+
+// segunda pratica guiada
+let a = 10
+let b = 7
+console.log(a<=b)
+
+let c = 332
+let d = 293
+console.log(c>=d)
+
+let e = 198
+let f = 198.1
+console.log(e>f)
+console.log(e<f)
+
+// terceira pratica guiada
+const fraseA = true
+const fraseB = true
+const fraseC = true
+const fraseD = true
+const fraseE = true
+
+const cenario1 = fraseA
+console.log('cenario 1:'${cenario1})
+const cenario2 = !fraseB
+console.log('Cenari 2:'${cenario2})
+const cenario3 = fraseB && !fraseE
+console.log('cenario3:'${cenario3})
+const cenario4 = fraseB && frasec || fraseD
+console.log('cenario 4:'${cenario4})
+const cenario5 = fraseB && fraseE && fraseA
+console.log('cenario 5'${cenario5})
+
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -7,3 +7,17 @@ const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+const afira = true
+const afirb = true
+const afirc = true
+const afird = true
+
+const operacao1 = !afirb && afird
+console.log('operacao1:'${operacao1})
+const operacao2 = afirb && afira && !afird
+console.log('operacao 2:'${operacao2})
+const operacao3 = afira || afird
+console.log('operacao3'${operacao3})
+const operacao4 = afirc && !afird
+const.log(operacao4:${operacao4})
